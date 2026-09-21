2d85d46542ee5e3fc199d87445548c4693eff535
admin@thiruadmin82gmailcom.onmicrosoft.com <admin@thiruadmin82gmailcom.onmicrosoft.com>
2026-09-21 18:19:04 +0000

    test from ds

--- notebooks/00_hello.py
+++ notebooks/00_hello.py
@@ -1,3 +1,7 @@
 # Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
 # Connectivity check: edit in Databricks, commit + push from the Git folder, see it on GitHub.
-print("connected")
+print("connected from databricks")
